d5f42723d616ef77c07cb90bc378b060d9781216
BILLKATO <biel.mk@hotmail.com>
2022-11-07 20:55:10 -0300

    TestingGif

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const GIFEncoder = require("gif-encoder-2");
 const fs = require("fs");
 
 try {
+    console.log(`Processo iniciado`);
+   // `who-to-greet` input defined in action metadata file
+   const nameToGreet = core.getInput('who-to-greet');
+   console.log(`Hello ${nameToGreet}!`);
+   const time = (new Date()).toTimeString();
+   core.setOutput("time", time);
+   // Get the JSON webhook payload for the event that triggered the workflow
+   const payload = JSON.stringify(github.context.payload, undefined, 2)
+   console.log(`The event payload: ${payload}`);
+      
     var size = 200;
-
     const canvas = cvs.createCanvas(size,size) // set the height and width of the canvas
     const ctx = canvas.getContext('2d');
+    console.log(`Canvas Criado`);
+
     const encoder = new GIFEncoder(size,size);
     encoder.setDelay(100);
     encoder.start(); // starts the encoder
+    console.log(`Enconder iniciado`);
 
     const background = () => {
         ctx.fillStyle = '##ff9505';
@@ -24,47 +36,46 @@ try {
      ctx.fillStyle = '#cc5803';
      ctx.fillRect(0, 0, slice, size);
      encoder.addFrame(ctx);
+     console.log(`Frame 1 concluido`);
      
      // frame 2
      background();
      ctx.fillStyle = '#e2711d';
      ctx.fillRect(slice, 0, slice, size);
      encoder.addFrame(ctx);
+     console.log(`Frame 2 concluido`);
      
      // frame 3
      background();
      ctx.fillStyle = '#fc7b03';
      ctx.fillRect(slice * 2, 0, slice, size);
      encoder.addFrame(ctx);
+     console.log(`Frame 3 concluido`);
      
      // frame 4
      background();
      ctx.fillStyle = '#ffb627';
      ctx.fillRect(slice * 3, 0, slice, size);
      encoder.addFrame(ctx);
+     console.log(`Frame 4 concluido`);
      
      // frame 5
      background();
      ctx.fillStyle = '#ffc971';
      ctx.fillRect(slice * 4, 0, slice, size);
      encoder.addFrame(ctx);
+     console.log(`Frame 5 concluido`);
      
      // end the encoding
      encoder.finish();
+     console.log(`Enconder Finalizado`);
 
      const buffer = encoder.out.getData();
      fs.writeFile('example.gif', buffer, error => {
         error ? console.log(error) : null;
-     });    
+     });
+     console.log(`Arquivo criado`);    
 
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
